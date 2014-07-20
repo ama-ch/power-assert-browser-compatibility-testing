bc8698a44c7c6241d20bae0f8c72c2d07f590c4b
ama-ch <amach97@gmail.com>
2014-07-20 19:11:06 +0900

    Setup SauceLabs and Travis CI.

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -2,6 +2,55 @@
 // Generated on Sun Jul 20 2014 16:36:51 GMT+0900 (JST)
 
 module.exports = function(config) {
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    console.error('SAUCE_USERNAME is not set.');
+    process.exit(1);
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    },
+    'SL_Firefox': {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '26'
+    },
+    'SL_Safari': {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    },
+    'SL_IE_8': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows XP',
+      version: '8'
+    },
+    'SL_IE_9': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2008',
+      version: '9'
+    },
+    'SL_IE_10': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 2012',
+      version: '10'
+    },
+    'SL_IE_11': {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -18,6 +67,7 @@ module.exports = function(config) {
       'node_modules/es5-shim/es5-shim.js',
       'node_modules/power-assert/build/power-assert.js'
     ],
+
 
     // list of files to browserify
     browserify: {
@@ -46,7 +96,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'saucelabs'],
 
 
     // web server port
@@ -62,17 +112,20 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    sauceLabs: {
+      testName: 'power-assert-borser-compatibility'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: Object.keys(customLaunchers),
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
