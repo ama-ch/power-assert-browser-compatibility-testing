9a3b9ed78497eed54385297ac7b2c1ae57cf3707
ama-ch <amach97@gmail.com>
2014-07-21 13:09:33 +0900

    Add test cases.

--- test/assert.spec.js
+++ test/assert.spec.js
@@ -2,5 +2,28 @@ describe('assert', function() {
   it('should work.', function() {
     assert(true);
     assert.ok(1 === 1);
+    assert.equal(1, '1');
+    assert.notEqual(0, 1);
+    assert.deepEqual([1, 2, 3], [1, 2, 3]);
+    assert.notDeepEqual([1, 2, 3], [1, 2, 4]);
+    assert.strictEqual(1, 1);
+    assert.notStrictEqual(1, '1');
+    assert.throws(function() {
+      throw new Error('Wrong value.');
+    }, /Wrong value/);
+    assert.doesNotThrow(function() {});
   });
+
+  it('should throw error.', function() {
+    try {
+      assert.fail('actual', 'expected', 'test message');
+      shouldNotCall();
+    } catch (e) {
+      assert(e.message === 'test message');
+    }
+  });
+
+  function shouldNotCall() {
+    throw new Error('This should not have been reached.');
+  }
 });
