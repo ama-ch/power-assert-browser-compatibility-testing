8e833faf46a2008b629b7b1345c0bbc0ab25d8f0
ama-ch <amach97@gmail.com>
2014-07-21 13:40:07 +0900

    Refine test cases.

--- test/assert.spec.js
+++ test/assert.spec.js
@@ -1,26 +1,73 @@
 describe('assert', function() {
   it('should work.', function() {
     assert(true);
-    assert.ok(1 === 1);
-    assert.equal(1, '1');
-    assert.notEqual(0, 1);
-    assert.deepEqual([1, 2, 3], [1, 2, 3]);
-    assert.notDeepEqual([1, 2, 3], [1, 2, 4]);
-    assert.strictEqual(1, 1);
-    assert.notStrictEqual(1, '1');
-    assert.throws(function() {
-      throw new Error('Wrong value.');
-    }, /Wrong value/);
-    assert.doesNotThrow(function() {});
   });
 
-  it('should throw error.', function() {
-    try {
-      assert.fail('actual', 'expected', 'test message');
-      shouldNotCall();
-    } catch (e) {
-      assert(e.message === 'test message');
-    }
+  describe('.ok()', function() {
+    it('should work.', function() {
+      assert.ok(1 === 1);
+    });
+  });
+
+  describe('.equal()', function() {
+    it('should work.', function() {
+      assert.equal(1, '1');
+    });
+  });
+
+  describe('.notEqual()', function() {
+    it('should work.', function() {
+      assert.notEqual(0, 1);
+    });
+  });
+
+  describe('.deepEqual()', function() {
+    it('should work.', function() {
+      assert.deepEqual([1, 2, 3], [1, 2, 3]);
+    });
+  });
+
+  describe('.notDeepEqual()', function() {
+    it('should work.', function() {
+      assert.notDeepEqual([1, 2, 3], [1, 2, 4]);
+    });
+  });
+
+  describe('.strictEqual()', function() {
+    it('should work.', function() {
+      assert.strictEqual(1, 1);
+    });
+  });
+
+  describe('.notStrictEqual()', function() {
+    it('should work.', function() {
+      assert.notStrictEqual(1, '1');
+    });
+  });
+
+  describe('.throws()', function() {
+    it('should work.', function() {
+      assert.throws(function() {
+        throw new Error('Wrong value.');
+      }, /Wrong value/);
+    });
+  });
+
+  describe('.doesNotThrow()', function() {
+    it('should work.', function() {
+      assert.doesNotThrow(function() {});
+    });
+  });
+
+  describe('.fail()', function() {
+    it('should throw error.', function() {
+      try {
+        assert.fail('actual', 'expected', 'test message');
+        shouldNotCall();
+      } catch (e) {
+        assert(e.message === 'test message');
+      }
+    });
   });
 
   function shouldNotCall() {
